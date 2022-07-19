c6f20dccb9b0795c9ef85af1afbd002d349aef64
Mylo Cyrus <irahiggins740@gmail.com>
2022-07-19 03:38:08 -0700

    Update Java.script.js

--- Java.script.js
+++ Java.script.js
@@ -1,1 +1,3 @@
-https://unpkg.com/embeddable-nfts@0.5.1/dist/nft-card.min.js
+3db73b998d536be0e1c2ec124df8e0f383ae7b602968ff6a5276ca0695023c46
+
+https://unpkg.com/embeddable-nfts@0.5.1/dist/3db73b998d536be0e1c2ec124df8e0f383ae7b602968ff6a5276ca0695023c46/nft-card.min.js
